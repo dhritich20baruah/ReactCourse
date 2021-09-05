3e1778ef0db7626417164d32d18204074501a7be
Delta <dhriti.baruah20@gmail.com>
2021-09-05 21:10:22 +0530

    lesson10

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+// import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 
@@ -9,6 +10,7 @@ function App() {
     <Navbar title="TextUtils" aboutText="About Us"/>
     <div className="container my-3">
     <TextForm heading="Enter the text to analyze"/>
+    {/* <About/> */}
     </div>
     </>
   );
